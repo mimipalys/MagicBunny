f94a2b0a04b8e4b48b5bb9938274cb6687633b87
erika <erika-lindberg97@hotmail.com>
2023-09-13 12:09:48 +0200

    structure

--- VacciMate/frontend/homepage/frontpage.php
+++ VacciMate/frontend/homepage/frontpage.php
@@ -3,43 +3,62 @@
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="boarderstyle.css">
-    <title>
+<style>
+body {background-color: powderblue;}
+h1   {color: red;}
+p    {color: red;}
+header div {
+
+  margin: 1rem;
+  padding: 2rem 2rem;
+  text-align: center;
+  float: left;
+}
+header a {
+
+margin: 1rem;
+padding: 2rem 2rem;
+text-align: center;
+float: left;
+}
+</style>
+ <link rel="stylesheet" type="text/css" href="/Users/erikaelisabet/VacciMate/MagicBunny/VacciMate/frontend/homepage/boarderstyle.css">
+ <title>
           Using display: flex and 
           justify-content: space-between
-</title>
+ </title>
 
 </head>
+
+
+
+
+
 <body>
-<header>
-    <div > <p> Syringe </p></div> 
-    <div > VacciMate </div> 
-    <div > Login </div> 
-    <div > Register </div> 
-    <div > settings </div> 
+ <h1>  hej </h1>
+ <header>
+  <div>
+    <div> syringe </div> 
+    <div> VacciMate </div> 
+    <a> Login </a> 
+    <div> Register </div> 
+    <div> settings </div> 
+  </div>
 
-<nav>
-
-</nav>
-</header>
-
-
-<main>
-    <div >
-    
-    </div>
-      
-    <div>
-    
-    </div>
-    <div>
-    
-    </div>
-</main>
-
-<footer>
-
-</footer>
+  <div>
+    <div> Travel Information </div> 
+    <div> Search Vaccine </div> 
+    <div> About Us</div> 
+  </div>
+  <nav>
 
 
+   </nav>
+
+ </header>
+
+
+
+
+</body>
 </html>
