c183349d2f274ef1125f2525136b146e2c3f4b3e
erika <erika-lindberg97@hotmail.com>
2023-09-20 09:15:49 +0200

    CSS style updated body is full screen

---
+++ VacciMate/frontend/About_Us/About_Us.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <link rel="stylesheet" type="text/css" href="../homepage/borderstyle.css">
+  <title>
+          Using display: flex and 
+          justify-content: space-between
+  </title>
+</head>
+
+<body>
+ <header>
+  <div class= "topheader">
+    <a class="vaccimateLogo"> &#128137 VacciMate </a> 
+    <div class= "rightpart_topheader">
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Login </a> 
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Register </a> 
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> &#9881 </a> 
+    </div>
+  </div>
+
+  <div class= "bottomheader">
+    <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton2"> Travel information </a> 
+    <a id = "GFG" href = "http://localhost:8888/processing/search_vaccine.php" class="costumbutton2"> Search Vaccine </a> 
+    <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton2"> About Us </a> 
+  </div>
+
+ </header>
+
+ <body>
+  <div class = "bodydiv">
+   <p> hejjj </p>
+  </div>
+
+ </body>
+
+</body>
+</html>
