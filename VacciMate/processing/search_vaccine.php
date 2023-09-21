9df8b9400f1be225e99c79fce60576e29d8e012a
JuliaAncker <julia.ancker@gmail.com>
2023-09-21 10:24:28 +0200

    hid the descrpition

--- VacciMate/processing/search_vaccine.php
+++ VacciMate/processing/search_vaccine.php
@@ -1,14 +1,62 @@
 
+
 <!DOCTYPE html>
 <html>
 
-    <body>
+<head>
+
+  <link rel="stylesheet" type="text/css" href="../frontend/homepage/borderstyle.css">
+  <title>
+          Using display: flex and 
+          justify-content: space-between
+  </title>
+</head>
+
+<body>
+ <header>
+  <div class= "topheader">
+    <a id = "GFG" class="vaccimateLogo" href = "http://localhost:8888/frontend/homepage/frontpage.php"> &#128137 VacciMate </a> 
+    <div class= "rightpart_topheader">
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Login </a> 
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Register </a> 
+     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> &#9881 </a> 
+    </div>
+  </div>
+  
+
+  <div class= "bottomheader">
+    <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton2"> Travel information </a> 
+    <p id = "GFG" class="costumbutton2_choosen"> Search Vaccine </p>  
+    <a id = "GFG" href = "http://localhost:8888/frontend/About_Us/About_Us.php" class="costumbutton2"> About Us </a> 
+  </div>
+
     <form method="POST">
-        <input type="text" name="query" placeholder="Vaccine Name or Disease...">
-        <button type="submit">Search</button>
+        <input type="text" name="query" placeholder="Vaccine Name or Disease..." >
+        <button type="submit">Search</button class="costumbutton1">
     </form>
 
+    <style>
+        /* Style for the description initially hidden */
+        .hidden-description {
+            display: none;
+        }
 
+        /* Style for the button */
+        .show-description-button {
+            cursor: pointer;
+        }
+
+        /* Style for the checkbox */
+        .show-description-checkbox {
+            display: none;
+        }
+
+        /* When the checkbox is checked, show the hidden description */
+        .show-description-checkbox:checked + .hidden-description {
+            display: block;
+        }
+    </style>
+    
     <?php
 // Database connection parameters
 $servername = "localhost";
@@ -45,11 +93,11 @@ $link->close();
     // creates buttons and description
 
     while($row = $result->fetch_assoc()) {
-        #$Vaccine_list[] = $row['VaccineName'];
-        echo '<details>';
-        echo '<summary>' . $row['VaccineName'] . '</summary>';
-        echo '<p>' . $row['Description'] . '</p>';
-        echo '</details>';
+        echo '<div>';
+        echo '<label for="show-description-' . $row['VaccineName'] . '" class="show-description-button">' . $row['VaccineName'] . '</label>';
+        echo '<input type="checkbox" id="show-description-' . $row['VaccineName'] . '" class="show-description-checkbox">';
+        echo '<p class="hidden-description">' . $row['Description'] . '</p>';
+        echo '</div>';
     }
     
     ?>
