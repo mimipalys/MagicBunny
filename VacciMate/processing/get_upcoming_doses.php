e49cf090ebad8346fa81af0e2d65afca75d51653
matiasWanntorp <119075087+matiasWanntorp@users.noreply.github.com>
2023-09-22 09:51:57 +0200

    added php file to get renewal interval for vaccinedoses

---
+++ VacciMate/processing/get_upcoming_doses.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// Database connection details
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = 'root';
+$dbName = 'VacciMate';
+
+// connect to db
+
+// prepare sql statement
+
+// fetch results
+
+// Vaccine Name
+// Dose number
+// interval of when to take it
+// only display if it is within a years time
+
+
+
+
+?>
