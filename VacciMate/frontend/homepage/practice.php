9c4163a2ebed45d2964439ed7102891e53198af1
mimipalys <65960805+mimipalys@users.noreply.github.com>
2023-09-24 23:11:15 +0200

    created practices css and php

---
+++ VacciMate/frontend/homepage/practice.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>VacciMate</title>
+        <link rel="stylesheet" type="text/css" href="practice.css">
+        </head>
+
+    <body>
+
+        <header>
+            <div class="center">
+                <a href="#">VacciMate</a>
+                <a href="#">login</a>
+                <a href="#">register</a>
+                <a href="#">settings</a>
+                </div>
+            <nav>
+
+                </nav>
+
+            </header>
+
+        <main>
+
+            </main>
+
+        <footer>
+
+            </footer>
+
+
+
+        </body>
+
+ </html>
