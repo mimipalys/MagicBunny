eafcc7e894e9a89fd6f39de4fb7109c0fa4b6e48
Zhiiiiiaozhao <emily.zhaolei@gmail.com>
2023-09-26 09:36:24 +0200

    update travel info

--- VacciMate/frontend/homepage/frontpage.php
+++ VacciMate/frontend/homepage/frontpage.php
@@ -23,7 +23,7 @@
   </div>
 
   <div class= "bottomheader">
-    <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton2"> Travel information </a> 
+    <a id = "GFG" href = "http://localhost:8888/processing/continents.php" class="costumbutton2"> Travel information </a> 
     <a id = "GFG" href = "http://localhost:8888/processing/search_vaccine.php" class="costumbutton2"> Search Vaccine </a> 
     <a id = "GFG" href = "http://localhost:8888/frontend/Non_connected_pages/About_Us/About_Us.php" class="costumbutton2"> About Us </a> 
   </div>
