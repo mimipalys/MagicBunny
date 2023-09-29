469c0d67294ef2ac0a9498adecf86ce01a2185e9
Zhiiiiiaozhao <emily.zhaolei@gmail.com>
2023-09-29 11:20:18 +0200

    Update countries.php

--- VacciMate/processing/countries.php
+++ VacciMate/processing/countries.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<?php
+  include('../frontend/links.php');
+?>
 <head>
     <link rel="stylesheet" type="text/css" href="http://localhost/frontend/borderstyle.css">
     <link rel="stylesheet" type="text/css" href="http://localhost/processing/country_page.css">
@@ -10,20 +13,27 @@
 </head>
 
 <body>
- <header>
+<header>
   <div class= "topheader">
-    <a id = "GFG" class="vaccimateLogo" href = "http://localhost:8888/frontend/homepage/frontpage.php"> &#128137 VacciMate </a> 
+    <?php
+     echo '<a id="GFG" class="vaccimateLogo" href="' . $homepage_link . '">&#128137 VacciMate</a>';
+    ?>
+    
     <div class= "rightpart_topheader">
-     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Login </a> 
-     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> Register </a> 
-     <a id = "GFG" href = "http://localhost:8888/processing/index.php" class="costumbutton1"> &#9881 </a> 
+    <?php
+     echo '<a id="GFG" href="' . $login_link . '" class="costumbutton1">Login</a>';
+     echo '<a id="GFG" href="' . $register_link . '"  class="costumbutton1">Register</a>';
+     echo '<a id="GFG" href="' . $setting_link . '" class="costumbutton1">&#9881</a>';
+    ?>
     </div>
   </div>
-
+  
   <div class= "bottomheader">
-  <a id = "GFG" href = "http://localhost:8888/processing/continents.php" class="costumbutton2"> Travel information </a> 
-    <a id = "GFG" href = "http://localhost:8888/processing/search_vaccine.php" class="costumbutton2"> Search Vaccine </a> 
-    <a id = "GFG" href = "http://localhost:8888/frontend/About_Us/About_Us.php" class="costumbutton2"> About Us </a>
+  <?php
+    echo '<a id="GFG" href="' . $travel_link . '" class="costumbutton2">Travel information</a>';
+    echo '<a id="GFG" href="' . $search_link . '"  class="costumbutton2">Search Vaccine</a>';
+    echo '<a id="GFG" href="' . $aboutUs_link . '"  class="costumbutton2">About Us</a>';
+  ?>
   </div>
 
  </header>
@@ -80,12 +90,27 @@
                     }
                 }
 
+                // Define an associative array to map keywords to their respective websites
+                $keywordToWebsite = array(
+                    "TBE" => "http://localhost/processing/search_vaccine.php?query=tbe",
+                    "hepatitis A" => "http://localhost/processing/search_vaccine.php?query=keyword2",
+                    "hepatitis B" => "http://localhost/processing/search_vaccine.php?query=keyword2",
+                    "tetanus" => "http://localhost/processing/search_vaccine.php?query=keyword3",
+                    "diphtheria" => "http://localhost/processing/search_vaccine.php?query=keyword3",
+                    "yellow fever" => "http://localhost/processing/search_vaccine.php?query=keyword2",
+                    "Japanese " => "http://localhost/processing/search_vaccine.php?query=keyword2",
+                );
+
                 // Loop through the target words and highlight them in the description
                 foreach ($targetWords as $word) {
-                    // Perform a case-insensitive search and replace
+                    // Check if the keyword exists in the mapping array
+                    if (isset($keywordToWebsite[$word])) {
+                    // Create a link to the corresponding website
+                        $vaccinelink = "<a href='" . $keywordToWebsite[$word] . "' class='highlight-link'>$word</a>";
+                    // Perform a case-insensitive search and replace with the link
                     $description = preg_replace(
                         "/\b" . preg_quote($word, '/') . "\b/i",
-                        "<a href='http://localhost/processing/search_vaccine.php'class='highlight-link'>$0</a>",
+                        $vaccinelink,
                         $description
                     );
                 }
