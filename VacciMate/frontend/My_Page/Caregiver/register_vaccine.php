7afa5131d655b4c47fc8047dde70ebff7db443ef
matiasWanntorp <119075087+matiasWanntorp@users.noreply.github.com>
2023-09-29 10:04:05 +0200

    Update register_vaccine.php

--- VacciMate/frontend/My_Page/Caregiver/register_vaccine.php
+++ VacciMate/frontend/My_Page/Caregiver/register_vaccine.php
@@ -9,7 +9,7 @@ echo $_SERVER["DOCUMENT_ROOT"];
 
 $caregiverID = $_SESSION['user_id'];
 
-// Check if the user is logged in; if not, redirect to the signIn.php page
+// Check if the user is logged in and is a caregiver; if not, redirect to the signIn.php page
 if (!isset($_SESSION['user_id']) or $_SESSION['role'] != "caregiver" ) {
     header("Location: ../../SignupandSingnin/signIn.php");
     exit;
