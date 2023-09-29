3b6101b290fd6ee3917e4b2979dd81f91924986f
Zhiiiiiaozhao <emily.zhaolei@gmail.com>
2023-09-29 21:21:38 +0200

    Update countries.php

--- VacciMate/processing/countries.php
+++ VacciMate/processing/countries.php
@@ -1,45 +1,43 @@
 <!DOCTYPE html>
 <html>
 <?php
-  include('../frontend/links.php');
+include('../frontend/links.php');
 ?>
 <head>
     <link rel="stylesheet" type="text/css" href="http://localhost:8888/frontend/borderstyle.css">
     <link rel="stylesheet" type="text/css" href="http://localhost:8888/processing/country_page.css">
     <title>
-          Using display: flex and 
-          justify-content: space-between
+        Using display: flex and
+        justify-content: space-between
     </title>
 </head>
 
 <body>
-<header>
-  <div class= "topheader">
-    <?php
-     echo '<a id="GFG" class="vaccimateLogo" href="' . $homepage_link . '">&#128137 VacciMate</a>';
-    ?>
-    
-    <div class= "rightpart_topheader">
-    <?php
-     echo '<a id="GFG" href="' . $login_link . '" class="costumbutton1">Login</a>';
-     echo '<a id="GFG" href="' . $register_link . '"  class="costumbutton1">Register</a>';
-     echo '<a id="GFG" href="' . $setting_link . '" class="costumbutton1">&#9881</a>';
-    ?>
-    </div>
-  </div>
-  
-  <div class= "bottomheader">
-  <?php
-    echo '<a id="GFG" href="' . $travel_link . '" class="costumbutton2">Travel information</a>';
-    echo '<a id="GFG" href="' . $search_link . '"  class="costumbutton2">Search Vaccine</a>';
-    echo '<a id="GFG" href="' . $aboutUs_link . '"  class="costumbutton2">About Us</a>';
-  ?>
-  </div>
+    <header>
+        <div class="topheader">
+            <?php
+            echo '<a id="GFG" class="vaccimateLogo" href="' . $homepage_link . '">&#128137 VacciMate</a>';
+            ?>
 
- </header>
+            <div class="rightpart_topheader">
+                <?php
+                echo '<a id="GFG" href="' . $login_link . '" class="costumbutton1">Login</a>';
+                echo '<a id="GFG" href="' . $register_link . '" class="costumbutton1">Register</a>';
+                echo '<a id="GFG" href="' . $setting_link . '" class="costumbutton1">&#9881</a>';
+                ?>
+            </div>
+        </div>
 
-    
-    
+        <div class="bottomheader">
+            <?php
+            echo '<a id="GFG" href="' . $travel_link . '" class="costumbutton2">Travel information</a>';
+            echo '<a id="GFG" href="' . $search_link . '" class="costumbutton2">Search Vaccine</a>';
+            echo '<a id="GFG" href="' . $aboutUs_link . '" class="costumbutton2">About Us</a>';
+            ?>
+        </div>
+
+    </header>
+
     <!-- Countries Section -->
     <section>
         <h1>Countries</h1>
@@ -48,7 +46,7 @@
         $servername = "localhost";
         $username = "root";
         $password = "root";
-        $dbname = "vaccimate"; 
+        $dbname = "vaccimate";
 
         // Get the selected continent from the URL parameter
         $selectedContinent = $_GET['continent'];
@@ -70,16 +68,18 @@
         // Check if any countries were found
         if ($result->num_rows > 0) {
             echo "<ul>";
+
             // Loop through the results and display countries and descriptions
             while ($row = $result->fetch_assoc()) {
                 $country = $row['Name'];
                 $description = $row['description'];
 
-                // Fetch the target words from the "vaccine" table 
+                // Fetch the target words from the "vaccine" table
                 $query = "SELECT RelatedDisease FROM vaccine";
                 $targetWordsResult = $link->query($query);
 
                 $targetWords = array();
+
                 if ($targetWordsResult->num_rows > 0) {
                     while ($targetRow = $targetWordsResult->fetch_assoc()) {
                         $vaccineName = $targetRow['RelatedDisease'];
@@ -90,27 +90,14 @@
                     }
                 }
 
-                // Define an associative array to map keywords to their respective websites
-                $keywordToWebsite = array(
-                    "TBE" => "http://localhost/processing/search_vaccine.php?query=tbe",
-                    "hepatitis A" => "http://localhost/processing/search_vaccine.php?query=keyword2",
-                    "hepatitis B" => "http://localhost/processing/search_vaccine.php?query=keyword2",
-                    "tetanus" => "http://localhost/processing/search_vaccine.php?query=keyword3",
-                    "diphtheria" => "http://localhost/processing/search_vaccine.php?query=keyword3",
-                    "yellow fever" => "http://localhost/processing/search_vaccine.php?query=keyword2",
-                    "Japanese " => "http://localhost/processing/search_vaccine.php?query=keyword2",
-                );
-
                 // Loop through the target words and highlight them in the description
                 foreach ($targetWords as $word) {
-                    // Check if the keyword exists in the mapping array
-                    if (isset($keywordToWebsite[$word])) {
-                    // Create a link to the corresponding website
-                        $vaccinelink = "<a href='" . $keywordToWebsite[$word] . "' class='highlight-link'>$word</a>";
+                    // Create a link to the search_vaccine page with the search_query parameter
+                    $vaccineLink = "http://localhost:8888/processing/search_vaccine.php?search_query=" . urlencode($word);
                     // Perform a case-insensitive search and replace with the link
                     $description = preg_replace(
                         "/\b" . preg_quote($word, '/') . "\b/i",
-                        $vaccinelink,
+                        "<a href='$vaccineLink' class='highlight-link'>$word</a>",
                         $description
                     );
                 }
