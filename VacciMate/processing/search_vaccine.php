770966e5df65f2edd2e0e1002f334f4f66fde612
JuliaAncker <julia.ancker@gmail.com>
2023-10-03 15:25:37 +0200

    changed things

--- VacciMate/processing/search_vaccine.php
+++ VacciMate/processing/search_vaccine.php
@@ -9,6 +9,7 @@
 <head>
 
   <link rel="stylesheet" type="text/css" href="../frontend/borderstyle.css">
+  <link rel="stylesheet" type="text/css" href="http://localhost:8888/processing/search_vaccine_page.css">
   <title>
           Using display: flex and 
           justify-content: space-between
@@ -38,9 +39,50 @@
     echo '<a id="GFG" href="' . $aboutUs_link . '"  class="costumbutton2">About Us</a>';
   ?>
   </div>
-</header>
+</html>
 
-    <style>
+
+<!DOCTYPE html>
+<html>
+<style>
+.Vaccine_title {
+            text-align: center;
+            font-size: 30px;
+            color: #333;
+            margin-top: 50px;
+        }
+        
+        /* Styles for the destination description */
+        .vaccine_text {
+            text-align: center;
+            font-size: 30px;
+            color: #666;
+            margin-top: 10px;
+            word-wrap: break-word; /* Allow automatic line wrapping */
+        }
+
+        .search-form {
+        text-align: center;
+        margin-top: 20px;
+        }
+
+        .search-form input[type="text"] {
+        padding: 10px;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        font-size: 16px;
+        }
+
+        .search-form input[type="submit"] {
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        padding: 10px 20px;
+        cursor: pointer;
+        font-size: 16px;
+        }
+
         /* Style for the description initially hidden */
         .search_vaccine {
             text-align: center;
@@ -110,71 +152,71 @@
         Height: auto;
         border-radius:20px;
         }
+</style>
+
+<link rel="stylesheet" type="text/css" href="http://localhost:8888/processing/search_vaccine_page.css">
+<header>
+    <section class="search_vaccine_page">   
+        <div  class = "search-form">
+            <form action="search_vaccine.php" method="GET">
+            <input class="search_vaccine" type="text" name="search_query" placeholder="Vaccine Name or Disease..." >
+            <input class = "search-form" type="submit" value="Search">
+            </form>
+        </div>
+        <h1 class = "Vaccine_title">Vaccine Information</h1>   
+        <p class = "vaccine_text"> Search for Vaccine or disease to find information or click vaccine name to read description</p>    
+
+
+        <?php
+        // Database connection parameters
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "VacciMate";
+
+        // Create connection
+        $link = mysqli_connect($servername, $username, $password, $dbname);
+
+        // Check if connection is established
+        if (mysqli_connect_error()) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+
+        if (isset($_GET['search_query'])) {
+            // Build the SQL query
+            $searchQuery = $_GET['search_query'];
+            $sql = "SELECT VaccineName, Description, RelatedDisease FROM Vaccine WHERE VaccineName LIKE '%$searchQuery%'";
+
+            // Create button that takes you back
+            echo '<div  class = "show-all-button">';
+            echo '<a id = "GFG" href = "http://localhost:8888/processing/search_vaccine.php" class="show-all-button"> Show all vaccines</a>';
+            echo '</div>';
+
+        }   else {
+            // Build the SQL query
+            $sql = "SELECT VaccineName, Description, RelatedDisease FROM Vaccine";
+            $test = 0;
+        }
+
+        // Create the vaccine buttons
+        $result = $link->query($sql);
+        while($row = $result->fetch_assoc()) {
+            echo '<div>';
+            echo '<label for="show-description-' . $row['VaccineName'] . '" class="show-description-button">' . $row['VaccineName'] . '</label>';
+            echo '<input type="checkbox" id="show-description-' . $row['VaccineName'] . '" class="show-description-checkbox">';
+            echo '<p class="vaccine_description1">'. "Vaccine Name: ".  $row['VaccineName']  ."<br><br>". "Related Disease: " . $row['RelatedDisease']."<br><br>". "Description: " .$row['Description'] . '</p>';
+            echo '</div>';
+        }
 
 
 
-    </style>
-    
-<!DOCTYPE html>
-<html>
-<body>
-    <div  class = "search_vaccine">
-        <form action="search_vaccine.php" method="GET">
-        <p class="search_vaccine">Search forccine</p> <input class="search_vaccine" type="text" name="search_query" placeholder="Vaccine Name or Disease..." >
-        <text-align: center><input type="submit" value="Search">
-        </form>
-    </div>
-
+        // Close the database connection
+        $link->close();
+        ?>
+    </section>
 </body>
-
-
-<?php
-// Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "VacciMate";
-
-// Create connection
-$link = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check if connection is established
-if (mysqli_connect_error()) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-
-if (isset($_GET['search_query'])) {
-    // Build the SQL query
-    $searchQuery = $_GET['search_query'];
-    $sql = "SELECT VaccineName, Description, RelatedDisease FROM Vaccine WHERE VaccineName LIKE '%$searchQuery%'";
-
-    // Create button that takes you back
-    echo '<div  class = "show-all-button">';
-    echo '<a id = "GFG" href = "http://localhost:8888/processing/search_vaccine.php" class="show-all-button"> Show all vaccines</a>';
-    echo '</div>';
-
-}   else {
-    // Build the SQL query
-    $sql = "SELECT VaccineName, Description, RelatedDisease FROM Vaccine";
-    $test = 0;
-}
-
-// Create the vaccine buttons
-$result = $link->query($sql);
-while($row = $result->fetch_assoc()) {
-    echo '<div>';
-    echo '<label for="show-description-' . $row['VaccineName'] . '" class="show-description-button">' . $row['VaccineName'] . '</label>';
-    echo '<input type="checkbox" id="show-description-' . $row['VaccineName'] . '" class="show-description-checkbox">';
-    echo '<p class="vaccine_description1">'. "Vaccine Name: ".  $row['VaccineName']  ."<br><br>". "Related Disease: " . $row['RelatedDisease']."<br><br>". "Description: " .$row['Description'] . '</p>';
-    echo '</div>';
-}
-
-
-
-// Close the database connection
-$link->close();
-?>
+</html>
 
 
 
