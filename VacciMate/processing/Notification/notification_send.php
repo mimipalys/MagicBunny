2db60d7ba5019acc5ed142ece58986a13ab6192b
erika <erika-lindberg97@hotmail.com>
2023-10-04 15:05:15 +0200

    email function added for notifications

--- VacciMate/processing/Notification/notification_send.php
+++ VacciMate/processing/Notification/notification_send.php
@@ -1,4 +1,12 @@
 <?php
+
+require 'SMTP.php';
+require 'PHPMailer.php';
+require 'Exception.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
 // Database connection details
 $dbHost = 'localhost';
 $dbUsername = 'root';
@@ -19,6 +27,8 @@ if ($db->connect_error) {
 $sql_specific_rows = "SELECT *
 FROM (
     SELECT 
+        `Fname`, 
+        `Lname`, 
         `AdministrationDate`, 
         `MailAddress`, 
         `PhoneNumber`, 
@@ -34,30 +44,64 @@ FROM (
     WHERE NotificationsEmail = 1 OR NotificationsPhone = 1
 ) AS subquery
 WHERE DATE_ADD(AdministrationDate, INTERVAL MinimumGap DAY) = DATE_ADD('2022-01-01', INTERVAL 30 DAY)";
+// use row below for real implementation!!
+// WHERE DATE_ADD(AdministrationDate, INTERVAL MinimumGap DAY) = DATE_ADD(CURDATE(), INTERVAL 30 DAY)";
 
 $result = $db->query($sql_specific_rows);
 
 // For each row in specific query if NotificationsEmail= 1 --> send email to MailAddress if NotificationsPhone = 1 --> send text to phone PhoneNumber
 // Assuming you have executed your SQL query and fetched the results into $result.
-
 while ($row = mysqli_fetch_assoc($result)) {
     if ($row['NotificationsEmail'] == 1) {
-        echo "To implement, sennding a notification to: " . $row['MailAddress'] . " ";
-        // Send an email to $row['MailAddress']
-        // You can use PHPMailer or another email library here
+        echo "To implement, sennding a notification to: " . str_replace(['"',"'"], "", $row['MailAddress']). " ";
+        
+        $mail = new PHPMailer(true); 
+        try {
+            $mail->SMTPDebug = 2;                                      
+            $mail->isSMTP();                                           
+            $mail->Host       = 'smtp.gmail.com;';                   
+            $mail->SMTPAuth   = true;                            
+            $mail->Username   = 'vaccimate0@gmail.com';                
+            $mail->Password   = 'qeptlkkgqfcjablf';                       
+            $mail->SMTPSecure = 'tls';                             
+            $mail->Port       = 587; 
+         
+            $mail->setFrom('VacciMate@gmail.com', 'VacciMate');   // Set sender of the mail
+            $mail->addAddress('VacciMate@gmail.com');   // Set receiver of email 
+    
+            $mail->isHTML(true);  
+            $mail->Subject = 'Vaccine Reminder';
+            $mail->Body    = 'Hi "' .  str_replace(['"',"'"], "", $row['Fname'] ) . '" "' .  $row['Lname'] . '" it is time for you to book take your "' .  $row['VaccineName'] . '" vaccine! Please try to do this within the next "' .  $row['MaximumGap'] . '" days!';
+            // $mail->AltBody = 'Body in plain text for non-HTML mail clients';
+            $mail->send();
+            echo "Mail has been sent successfully according to schedule!";
+        } 
+        
+        catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 
     if ($row['NotificationsPhone'] == 1) {
-        echo "HI Phone"; 
+        // echo "HI Phone"; 
         // Send a text message to $row['PhoneNumber']
         // You would need to use a text messaging service or API for this
     }
 }
 
+
+
+
+
+
+
+
 // The real deal
 // $sql_specific_rows = "SELECT *
 // FROM (
 //     SELECT 
+//         `Fname`, 
+//         `Lname`, 
 //         `AdministrationDate`, 
 //         `MailAddress`, 
 //         `PhoneNumber`, 
@@ -73,11 +117,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 //     WHERE NotificationsEmail = 1 OR NotificationsPhone = 1
 // ) AS subquery
 // WHERE DATE_ADD(AdministrationDate, INTERVAL MinimumGap DAY) = DATE_ADD(CURDATE(), INTERVAL 30 DAY)";
-
-
-
-
-
 
 
 ?>
