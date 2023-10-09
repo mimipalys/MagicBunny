91be44fb5a556aeabe3f6a5a4563c0bd2fa41f6b
matiasWanntorp <119075087+matiasWanntorp@users.noreply.github.com>
2023-10-09 15:45:07 +0200

    added placeholder text for news items

--- VacciMate/frontend/Non_connected_pages/News/TBE.php
+++ VacciMate/frontend/Non_connected_pages/News/TBE.php
@@ -38,12 +38,17 @@
 
  </header>
  <body>
- <h1> TBE is bad </h1>
  <div class = "bodydiv">
    <div class = "Body_fullscreen">
-     <h2> TBE </h2>
-     <p> Information about the vaccine...  </p>
-   </div>
+     <h1> Did you remember to take your TBE - vaccine? </h1>
+     <h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu risus quis varius quam. Dolor sit amet consectetur adipiscing. Non consectetur a erat nam at lectus urna duis convallis. Leo vel fringilla est ullamcorper eget nulla. Fringilla phasellus faucibus scelerisque eleifend donec. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Aliquam id diam maecenas ultricies mi eget mauris. Pulvinar pellentesque habitant morbi tristique senectus et. Euismod lacinia at quis risus sed. In nisl nisi scelerisque eu ultrices vitae. Id nibh tortor id aliquet lectus proin nibh nisl. Sed turpis tincidunt id aliquet risus feugiat in. Fermentum et sollicitudin ac orci. Sapien faucibus et molestie ac. Nullam non nisi est sit. Odio ut enim blandit volutpat maecenas. Duis ultricies lacus sed turpis tincidunt. Quis auctor elit sed vulputate mi sit amet mauris commodo.
+     </h4>
+<p>Tellus elementum sagittis vitae et leo duis ut diam. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Vitae suscipit tellus mauris a. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. At auctor urna nunc id cursus metus aliquam eleifend. Eu augue ut lectus arcu bibendum at varius. Lacus sed turpis tincidunt id aliquet risus feugiat in. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Elementum curabitur vitae nunc sed velit dignissim. Auctor neque vitae tempus quam. Sit amet purus gravida quis blandit turpis cursus. Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Arcu odio ut sem nulla pharetra. Tortor consequat id porta nibh venenatis cras sed felis. In hac habitasse platea dictumst vestibulum rhoncus est. Eget nullam non nisi est sit. Amet massa vitae tortor condimentum lacinia quis vel eros.
+</p>
+<p>
+Quam quisque id diam vel quam elementum. Tristique senectus et netus et malesuada fames ac. Turpis egestas integer eget aliquet nibh. Eu non diam phasellus vestibulum. Risus sed vulputate odio ut enim blandit volutpat maecenas. Leo in vitae turpis massa sed. Diam maecenas sed enim ut. Tortor at risus viverra adipiscing at in tellus integer feugiat. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Diam vulputate ut pharetra sit amet aliquam. Vitae suscipit tellus mauris a diam maecenas sed. Integer eget aliquet nibh praesent tristique magna sit amet. Vitae nunc sed velit dignissim sodales ut eu sem. Nulla at volutpat diam ut venenatis tellus in metus. Purus sit amet luctus venenatis lectus magna fringilla urna. Lectus urna duis convallis convallis tellus.  </p>
+</p>  
+</div>
   </div>
 
  </body>
--- VacciMate/frontend/Non_connected_pages/News/influenza.php
+++ VacciMate/frontend/Non_connected_pages/News/influenza.php
@@ -38,11 +38,16 @@
 
  </header>
  <body>
- <h1> Influenza </h1>
  <div class = "bodydiv">
    <div class = "Body_fullscreen">
-     <h2> Influenza </h2>
-     <p> Traveling information, what to think about </p>
+     <h2> Some exciting news on the progress of the new influenza vaccine! </h2>
+     <h4>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat. Vulputate dignissim suspendisse in est. In mollis nunc sed id semper risus in. Tortor at auctor urna nunc id cursus metus. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Non nisi est sit amet facilisis magna etiam tempor orci. Massa enim nec dui nunc mattis enim ut tellus elementum. Vehicula ipsum a arcu cursus vitae congue mauris. Arcu odio ut sem nulla pharetra diam sit. Ultrices vitae auctor eu augue ut lectus. At augue eget arcu dictum varius duis. Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Leo a diam sollicitudin tempor id.
+</h4>
+<p>
+    Vulputate ut pharetra sit amet aliquam id diam maecenas. Arcu non sodales neque sodales. Pulvinar sapien et ligula ullamcorper malesuada proin. Condimentum mattis pellentesque id nibh tortor id aliquet. Varius quam quisque id diam. Sed sed risus pretium quam vulputate dignissim suspendisse in est. At tellus at urna condimentum mattis pellentesque id nibh tortor. Felis eget velit aliquet sagittis id consectetur. Aliquam etiam erat velit scelerisque in. Tristique senectus et netus et malesuada. Gravida cum sociis natoque penatibus et. Lacus sed viverra tellus in hac. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Ac feugiat sed lectus vestibulum. Nisl vel pretium lectus quam id leo. Pellentesque habitant morbi tristique senectus et. Ornare lectus sit amet est placerat in. Amet nulla facilisi morbi tempus iaculis. Enim sed faucibus turpis in eu mi bibendum neque. Imperdiet nulla malesuada pellentesque elit.
+</p>
+
    </div>
   </div>
 
