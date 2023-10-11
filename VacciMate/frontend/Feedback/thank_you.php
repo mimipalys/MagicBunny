56bfa1a88ec7fa6053ff346b6408860cfec550f9
PrasannaVenkat04 <prasannav0412@gmail.com>
2023-10-11 16:39:49 +0200

    commit

--- VacciMate/frontend/Feedback/thank_you.php
+++ VacciMate/frontend/Feedback/thank_you.php
@@ -2,6 +2,15 @@
 <html>
 
 <head>
+  <link rel="stylesheet" type="text/css" href="borderstyle.css">
+
+  <title>
+          Using display: flex and 
+          justify-content: space-between
+  </title>
+</head>
+
+<head>
     <title>Thank You for Your Feedback</title>
 </head>
 
@@ -10,7 +19,7 @@
 
     <p>We appreciate your feedback regarding your vaccination experience. Your input helps us improve our services.</p>
 
-    <p><a href="feedback.php">Go back to the feedback form</a></p>
+    <p><a href="feedback.html">Go back to the feedback form</a></p>
 </body>
 
 </html>
