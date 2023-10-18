8dd2edeebdebb9220e601b972b8c5bde27659fd6
Zhiiiiiaozhao <emily.zhaolei@gmail.com>
2023-10-18 23:01:57 +0200

    small changes

--- VacciMate/processing/countries.php
+++ VacciMate/processing/countries.php
@@ -3,6 +3,7 @@
 <?php
 include('../frontend/links.php');
 ?>
+
 <head>
     <link rel="stylesheet" type="text/css" href="../frontend/borderstyle.css">
     <link rel="stylesheet" type="text/css" href="country_page.css">
@@ -15,7 +16,6 @@ include('../frontend/links.php');
 <?php 
 session_start();
 
-//include correct header
 if (isset($_SESSION['user_id']) and $_SESSION['role'] == "patient") {
   include $header_logged_in_patient;
 } elseif (isset($_SESSION['user_id']) and $_SESSION['role'] == "caregiver") {
@@ -24,87 +24,88 @@ if (isset($_SESSION['user_id']) and $_SESSION['role'] == "patient") {
   include $header;
 }
 
-
 ?>
 
-    <!-- Countries Section -->
-    <section>
-        <a href="continents.php" style='font-size:50px; font-family: Zapf Dingbats;'>&#8592;</a>
-        <h1>Countries</h1>
-        <?php
-        // Database connection parameters
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "vaccimate";
+<!-- Countries Section -->
+<section>
+    <a href="continents.php" style='font-size:50px; font-family: Zapf Dingbats;'>&#8592;</a>
+    <h1>Countries</h1>
+    <?php
+    // Database connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "vaccimate";
 
-        // Get the selected continent from the URL parameter
-        $selectedContinent = $_GET['continent'];
+    // Get the selected continent from the URL parameter
+    $selectedContinent = $_GET['continent'];
 
-        // Create connection
-        $link = mysqli_connect($servername, $username, $password, $dbname);
+    // Create connection
+    $link = mysqli_connect($servername, $username, $password, $dbname);
 
-        // Check if connection is established
-        if (mysqli_connect_error()) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+    // Check if connection is established
+    if (mysqli_connect_error()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-        // SQL query to fetch countries and descriptions for the selected continent
-        $sql = "SELECT Name, description FROM Country WHERE continent = '$selectedContinent'";
 
-        // Execute the query
-        $result = $link->query($sql);
+    $sql = "SELECT Name, description FROM Country WHERE continent = ?";
+    $stmt = $link->prepare($sql);
+    $stmt->bind_param("s", $selectedContinent);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        // Check if any countries were found
-        if ($result->num_rows > 0) {
-            echo "<ul>";
+    // Check if any countries were found
+    if ($result->num_rows > 0) {
+        echo "<ul>";
 
-            // Loop through the results and display countries and descriptions
-            while ($row = $result->fetch_assoc()) {
-                $country = $row['Name'];
-                $description = $row['description'];
+        // Loop through the results and display countries and descriptions
+        while ($row = $result->fetch_assoc()) {
+            $country = $row['Name'];
+            $description = $row['description'];
 
-                // Fetch the target words from the "vaccine" table
-                $query = "SELECT RelatedDisease FROM vaccine";
-                $targetWordsResult = $link->query($query);
+            // Fetch the target words from the "vaccine" table
+            $query = "SELECT RelatedDisease FROM vaccine";
+            $targetWordsResult = $link->query($query);
 
-                $targetWords = array();
+            $targetWords = array();
 
-                if ($targetWordsResult->num_rows > 0) {
-                    while ($targetRow = $targetWordsResult->fetch_assoc()) {
-                        $vaccineName = $targetRow['RelatedDisease'];
-                        // Split the vaccine name into individual words
-                        $vaccineWords = explode(', ', $vaccineName);
-                        // Add each word to the targetWords array
-                        $targetWords = array_merge($targetWords, $vaccineWords);
-                    }
+            if ($targetWordsResult->num_rows > 0) {
+                while ($targetRow = $targetWordsResult->fetch_assoc()) {
+                    $vaccineName = $targetRow['RelatedDisease'];
+                    // Split the vaccine name into individual words
+                    $vaccineWords = explode(', ', $vaccineName);
+                    // Add each word to the targetWords array
+                    $targetWords = array_merge($targetWords, $vaccineWords);
                 }
-
-                // Loop through the target words and highlight them in the description
-                foreach ($targetWords as $word) {
-                    // Create a link to the search_vaccine page with the search_query parameter
-                    $vaccineLink = "http://localhost:8888/processing/search_vaccine/search_vaccine.php?search_query=" . urlencode($word);
-                    // Perform a case-insensitive search and replace with the link
-                    $description = preg_replace(
-                        "/\b" . preg_quote($word, '/') . "\b/i",
-                        "<a href='$vaccineLink' class='highlight-link'>$word</a>",
-                        $description
-                    );
-                }
-
-                echo "<li><strong>$country</strong></li>";
-                echo "<li>$description</li>";
             }
-            echo "</ul>";
-        } else {
-            echo "No countries found for $selectedContinent.";
-        }
 
-        // Close the database connection
-        $link->close();
-        ?>
-    </section>
-    </body>
+            // Loop through the target words and highlight them in the description
+            foreach ($targetWords as $word) {
+                // Create a link to the search_vaccine page with the search_query parameter
+                $vaccineLink = "http://localhost:8888/processing/search_vaccine/search_vaccine.php?search_query=" . urlencode($word);
+                // Perform a case-insensitive search and replace with the link
+                $description = preg_replace(
+                    "/\b" . preg_quote($word, '/') . "\b/i",
+                    "<a href='$vaccineLink' class='highlight-link'>$word</a>",
+                    $description
+                );
+            }
+
+            echo "<li><strong>$country</strong></li>";
+            echo "<li>$description</li>";
+        }
+        echo "</ul>";
+    } else {
+        echo "No countries found for $selectedContinent.";
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $link->close();
+    ?>
+</section>
+</body>
 <?php
 include $footer;
 ?>
--- VacciMate/processing/search_country.php
+++ VacciMate/processing/search_country.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
 <?php
-  include('../frontend/links.php');
+include('../frontend/links.php');
 ?>
-
 
 <head>
     <link rel="stylesheet" type="text/css" href="<?php echo $continents_style ?>">
     <link rel="stylesheet" type="text/css" href="http://localhost/processing/country_page.css">
     <title>
-          Using display: flex and 
-          justify-content: space-between
+        Using display: flex and 
+        justify-content: space-between
     </title>
 </head>
 
@@ -18,93 +17,93 @@
 <?php 
 session_start();
 
-//include correct header
 if (isset($_SESSION['user_id']) and $_SESSION['role'] == "patient") {
-  include $header_logged_in_patient;
+    include $header_logged_in_patient;
 } elseif (isset($_SESSION['user_id']) and $_SESSION['role'] == "caregiver") {
-  include $header_logged_in_caregiver;
+    include $header_logged_in_caregiver;
 } else {
-  include $header;
+    include $header;
 }
 
-
 ?>
-    <!-- Countries Section -->
-    <section class = 'vaccine_description1'>
-        <h1>Countries</h1>
-        <?php
-        // Database connection parameters
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "vaccimate"; // Update with your actual database name
+<!-- Countries Section -->
+<section class="vaccine_description1">
+    <h1>Countries</h1>
+    <?php
+    // Database connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "vaccimate"; // Update with your actual database name
 
-        // Get the selected continent from the URL parameter
-        $selectedCountry = $_GET['country'];
+    // Get the selected continent from the URL parameter
+    $selectedCountry = $_GET['country'];
 
-        // Create connection
-        $link = mysqli_connect($servername, $username, $password, $dbname);
+    // Create connection
+    $link = mysqli_connect($servername, $username, $password, $dbname);
 
-        // Check if connection is established
-        if (mysqli_connect_error()) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+    // Check if connection is established
+    if (mysqli_connect_error()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-        // SQL query to fetch countries and descriptions for the selected continent
-        $sql = "SELECT Name, description FROM Country WHERE Name LIKE '%$selectedCountry%'";
+    $sql = "SELECT Name, description FROM Country WHERE Name LIKE ?";
+    $stmt = $link->prepare($sql);
+    $searchQuery = '%' . $selectedCountry . '%';
+    $stmt->bind_param("s", $searchQuery);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        // Execute the query
-        $result = $link->query($sql);
+    // Check if any countries were found
+    if ($result->num_rows > 0) {
+        echo "<ul>";
+        // Loop through the results and display countries and descriptions
+        while ($row = $result->fetch_assoc()) {
+            $country = $row['Name'];
+            $description = $row['description'];
 
-        // Check if any countries were found
-        if ($result->num_rows > 0) {
-            echo "<ul>";
-            // Loop through the results and display countries and descriptions
-            while ($row = $result->fetch_assoc()) {
-                $country = $row['Name'];
-                $description = $row['description'];
+            // Fetch the target words from the "vaccine" table 
+            $query = "SELECT RelatedDisease FROM vaccine";
+            $targetWordsResult = $link->query($query);
 
-                // Fetch the target words from the "vaccine" table 
-                $query = "SELECT RelatedDisease FROM vaccine";
-                $targetWordsResult = $link->query($query);
-
-                $targetWords = array();
-                if ($targetWordsResult->num_rows > 0) {
-                    while ($targetRow = $targetWordsResult->fetch_assoc()) {
-                        $vaccineName = $targetRow['RelatedDisease'];
-                        // Split the vaccine name into individual words
-                        $vaccineWords = explode(', ', $vaccineName);
-                        // Add each word to the targetWords array
-                        $targetWords = array_merge($targetWords, $vaccineWords);
-                    }
+            $targetWords = array();
+            if ($targetWordsResult->num_rows > 0) {
+                while ($targetRow = $targetWordsResult->fetch_assoc()) {
+                    $vaccineName = $targetRow['RelatedDisease'];
+                    // Split the vaccine name into individual words
+                    $vaccineWords = explode(', ', $vaccineName);
+                    // Add each word to the targetWords array
+                    $targetWords = array_merge($targetWords, $vaccineWords);
                 }
-
-                // Loop through the target words and highlight them in the description
-                foreach ($targetWords as $word) {
-                    // Create a link to the search_vaccine page with the search_query parameter
-                    $vaccineLink = "http://localhost:8888/processing/search_vaccine/search_vaccine.php?search_query=" . urlencode($word);
-                    // Perform a case-insensitive search and replace with the link
-                    $description = preg_replace(
-                        "/\b" . preg_quote($word, '/') . "\b/i",
-                        "<a href='$vaccineLink' class='highlight-link'>$word</a>",
-                        $description
-                    );
-                }
-
-                echo "<li><strong>$country</strong></li>";
-                echo "<li>$description</li>";
             }
-            echo "</ul>";
-        } else {
-            echo "No countries found for $selectedContinent.";
-        }
 
-        // Close the database connection
-        $link->close();
-        ?>
-    </section>
+            // Loop through the target words and highlight them in the description
+            foreach ($targetWords as $word) {
+                // Create a link to the search_vaccine page with the search_query parameter
+                $vaccineLink = "http://localhost:8888/processing/search_vaccine/search_vaccine.php?search_query=" . urlencode($word);
+                // Perform a case-insensitive search and replace with the link
+                $description = preg_replace(
+                    "/\b" . preg_quote($word, '/') . "\b/i",
+                    "<a href='$vaccineLink' class='highlight-link'>$word</a>",
+                    $description
+                );
+            }
+
+            echo "<li><strong>$country</strong></li>";
+            echo "<li>$description</li>";
+        }
+        echo "</ul>";
+    } else {
+        echo "No countries found for $selectedCountry.";
+    }
+
+    // Close the statement and database connection
+    $stmt->close();
+    $link->close();
+    ?>
+</section>
 </body>
 <?php 
- include $footer;
- ?>
+include $footer;
+?>
 </html>
